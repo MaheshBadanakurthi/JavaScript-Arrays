81054ff4ea1aaae74306dc36f307141a181a3979
Mahesh <itstribe.mahi@gmail.com>
2022-08-16 19:02:28 +0530

    Arrays concept

---
+++ arrays.js
@@ -0,0 +1,20 @@
+var x=['Car', 123,567,'Jaguar', 'tiger'];
+// len=x.length;  // used to know length of array
+// console.log(len); // printing length
+// console.log(x[2]);// accessing a perticular element
+// console.log(x[0]);
+// console.log(x[7]);
+
+// console.log(len-4);
+
+x.push('TVS',2345);  // adding items to array
+len2=x.length;
+console.log(len2);
+
+console.log(x);
+x.unshift('TVS');
+console.log(len2);
+console.log(x);
+x.pop('Jaguar');
+console.log(x);
+
