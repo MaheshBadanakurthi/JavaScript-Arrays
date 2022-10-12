8f77c4d69e6b3ec31ddfc7b122ce19fe74966a70
Mahesh <itstribe.mahi@gmail.com>
2022-10-12 18:40:55 +0530

    Some arrayn practises

--- arrays.js
+++ arrays.js
@@ -18,3 +18,18 @@ console.log(x);
 x.pop('Jaguar');
 console.log(x);
 
+
+// one array contains multiple arrays we converted to single array
+let shortArr=[
+    [12,345,'naresh',678,9,0,34],
+    [324,4,546,78,89,90,09],
+    [21345,56,7,89,900],
+    ['mahi','nani','rani','madhupriya','navya',['raju','ajay','shekar','pa1']]
+]
+let shot=shortArr.reduce((accum,curElm)=>{
+    return accum.concat(curElm);
+})
+console.log(shot)
+
+
+
